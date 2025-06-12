838dad1596293669e2f8cfb608314153778d6f3f
b27user12 <b27user12@rpsconsulting.in>
2025-06-13 00:34:07 +0530

    helloworldtest

---
+++ Conversation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1 = modi
+person2 = sameer
+
+echo "$person1: hey how are you $person2?"
+echo "$person2: i am fine sir"
+echo "$person2: its been a pleasure to meet you sir"
+echo "$person1: whats the idea you have $person2?"
+echo "$person2: i have an idea for space agriculture and qunatum computing"
+echo "$person1: lets make bharat proud"
