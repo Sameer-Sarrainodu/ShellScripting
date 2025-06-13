73a1828f778921db70e07c2ca49a1c0310b0b34f
b27user12 <b27user12@rpsconsulting.in>
2025-06-13 20:03:29 +0530

    exitstatustest

---
+++ Exiting.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+userid=$(id -u)
+if [ $userid -eq 0 ]
+then
+    echo "matched success"
+else
+    echo "unsuccess"
+    exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "my sql not installed"
+    dnf install mysql -y
+    if[ $? -eq 0 ]
+    then
+    echo "it is successful"
+    else
+    echo "mysql is not installed"
+    fi
+else
+    echo "it is installed already"
+fi
+
+
